ea195f544a849afa30dc1c510ac8dbd3b5595fe0
ChissF <theyaemoribot@gmail.com>
2024-08-30 10:11:50 -0500

    Actualizar owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -5,7 +5,7 @@ let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
 const owner = 'Dev-Diego';
-const repo = 'YaemoriBot-MD';
+const repo = 'Bot-Tiburon-MD';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
  conn.sendMessage(m.chat, {text: `*✅️ Comando activado con éxito, te notificaremos cuando se actualize el repositorio.*`}, {quoted: estilo});  
 try {
