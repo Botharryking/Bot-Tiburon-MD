f9fd0b9047c9d496a383d01383d35042f2d8ab62
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-08-30 21:33:57 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -48,7 +48,7 @@ global.catalogo = fs.readFileSync('./media/catalogo.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.grupo = ''
+global.grupo = 'https://chat.whatsapp.com/LJx0VZk3Oko5mBe9I3r9BH'
 global.channel = 'https://whatsapp.com/channel/0029VaQD7LAJP216tu9liI2A'
 global.channel2 = 'https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z'
 global.md = 'https://github.com/Dev-Diego/Bot-Tiburon-MD' 
